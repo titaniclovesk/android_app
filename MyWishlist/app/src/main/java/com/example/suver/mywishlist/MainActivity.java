9c7d3ef46b88d25a4dc9e99ec3d9781dbff0a6c9
Aarti Verma <aartichoudhary21@gmail.com>
2016-10-11 17:39:03 +0530

    initial commit

---
+++ MyWishlist/app/src/main/java/com/example/suver/mywishlist/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.suver.mywishlist;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.setWebViewClient(new WebViewClient());
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        myWebView.loadUrl("https://wishlist-serverapp.herokuapp.com/");
+    }
+}
